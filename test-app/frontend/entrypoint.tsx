3b7f107023ab540d943c83cfdc6bf0902497c4d3
Benedikt Strehle <benedikt.strehle@uni-ulm.de>
2023-10-06 23:41:10 +0200

    add persistent component test

--- test-app/frontend/entrypoint.tsx
+++ test-app/frontend/entrypoint.tsx
@@ -3,12 +3,17 @@ import { testComponents } from "../common/test-components.tsx";
 import { invalid } from "../common/errors.tsx";
 import { HTTPError } from "uix/html/http-error.ts";
 import { HTTPStatus } from "uix/html/http-status.ts";
+import { HelloComponent } from "uix/test-app/common/HelloComponent.tsx";
+
+const comp = await lazyEternalVar('persistent') ?? $$(<HelloComponent name="World42"/>);
+console.log("comp", comp)
 
 export default {
 	'/:component/frontend': (ctx, {component}) => testComponents[component as keyof typeof testComponents] || new HTTPError(HTTPStatus.NOT_FOUND),
 	'/:component/backend*': null,
 	'/x/*': {
-		'/lazy': () => import("./lazy.tsx")
+		'/lazy': () => import("./lazy.tsx"),
+		'/persistent': () => comp
 	},
 
 	'frontendError': (ctx) => {
@@ -27,6 +32,7 @@ export default {
 		const val = sharedData[key];
 		return UIX.renderStatic(`${key}=${val}`)
 	}) satisfies UIX.Entrypoint,
+
 
 	'*': invalid
 } satisfies UIX.Entrypoint;

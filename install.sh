03d09c01e6ca9c8de8b7cce252eaa33c241c645c
Benedikt Strehle <benedikt.strehle@uni-ulm.de>
2023-10-17 14:18:07 +0200

    fix :display

--- install.sh
+++ install.sh
@@ -1,8 +1,10 @@
 # Copyright 2023 unyt.org
 
 # URLs
-uix_script_url="https://dev.cdn.unyt.org/uix/run.sh"
-uix_importmap_url="https://dev.cdn.unyt.org/importmap.json"
+uix_script_url="https://cdn.unyt.org/uix/run.ts"
+uix_importmap_url="https://cdn.unyt.org/uix/importmap.json"
 
 # install uix via deno installer
-deno install --importmap $uix_importmap_url -Aqr -n uix $uix_script_url
+deno install --import-map $uix_importmap_url -Aqr -n uix $uix_script_url
+
+# deno install --import-map https://dev.cdn.unyt.org/uix1/importmap.dev.json -Aqr -n uix1 https://dev.cdn.unyt.org/uix1/run.ts

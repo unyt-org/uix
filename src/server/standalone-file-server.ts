d1e2defcfd5f752be51fc526b99eb100a7b66325
Benedikt Strehle <benedikt.strehle@uni-ulm.de>
2023-10-18 19:21:20 +0200

    update importmap, submodule

--- src/server/standalone-file-server.ts
+++ src/server/standalone-file-server.ts
@@ -5,7 +5,7 @@ await Deno.run({
         "-Aq",
         "--import-map",
         "https://dev.cdn.unyt.org/importmap.json",
-        "https://dev.cdn.unyt.org/uix/server/file-server-runner.ts",
+        "https://dev.cdn.unyt.org/uix1/src/server/file-server-runner.ts",
         ...Deno.args
     ]
 }).status()

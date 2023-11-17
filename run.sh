f81f6d7d33488874100f3d32fc020fa8b18d4bca
benStre <strehle.benedikt@gmail.com>
2023-11-17 18:07:38 +0100

    add run.sh

---
+++ run.sh
@@ -0,0 +1,6 @@
+# URLs
+uix_script_url="https://cdn.unyt.org/uix/run.ts"
+uix_importmap_url="https://cdn.unyt.org/uix/importmap.json"
+
+# install uix via deno installer
+deno run --import-map $uix_importmap_url -Aqr $uix_script_url

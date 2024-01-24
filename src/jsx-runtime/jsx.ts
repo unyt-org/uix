8d43fc8441cc7ba58c6e454a5816048a3c56c046
benStre <strehle.benedikt@gmail.com>
2024-01-24 23:23:11 +0100

    fix: globalThis._jsx writable, fix weboscket com interface error

--- src/jsx-runtime/jsx.ts
+++ src/jsx-runtime/jsx.ts
@@ -9,7 +9,7 @@ export const Fragment = _Fragment;
 
 // make jsx available on globalThis
 Object.defineProperties(globalThis, {
-	_jsx: {value: jsx},
-	_jsxs: {value: jsxs},
-	_Fragment: {value: Fragment},
+	_jsx: {value: jsx, writable: true},
+	_jsxs: {value: jsxs, writable: true},
+	_Fragment: {value: Fragment, writable: true},
 })

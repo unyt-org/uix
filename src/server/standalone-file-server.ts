1f011986e95b3a81d7c384f3efea647131827a6d
Benedikt Strehle <benedikt@unyt.org>
2024-01-25 16:19:05 +0100

    Revert "Add deno config with experimentalDecorators"

--- src/server/standalone-file-server.ts
+++ src/server/standalone-file-server.ts
@@ -5,9 +5,7 @@ await Deno.run({
         "-Aq",
         "--import-map",
         "https://dev.cdn.unyt.org/importmap.json",
-        "--config",
-        "https://dev.cdn.unyt.org/deno.json",
-        "https://cdn.unyt.org/uix/src/server/file-server-runner.ts",
+        "https://dev.cdn.unyt.org/uix1/src/server/file-server-runner.ts",
         ...Deno.args
     ]
 }).status()

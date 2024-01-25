f86c55c215e2fee5ac22501dcc69c70adad6286b
benStre <strehle.benedikt@gmail.com>
2024-01-25 15:59:41 +0100

    update install.sh

--- install.sh
+++ install.sh
@@ -6,4 +6,4 @@ uix_importmap_url="https://cdn.unyt.org/uix/importmap.json"
 deno_config_url="https://dev.cdn.unyt.org/deno.json"
 
 # install uix via deno installer
-deno install --import-map $uix_importmap_url -Aq -n uix $uix_script_url
+deno install --import-map $uix_importmap_url --config $deno_config_url -Aq -n uix $uix_script_url

afb72e92ddfe876ba6a7559904051e1b67e04bb8
benStre <strehle.benedikt@gmail.com>
2024-01-27 02:20:19 +0100

    improve error messages, handle stream timeouts

--- src/runners/run-local-docker.ts
+++ src/runners/run-local-docker.ts
@@ -1,6 +1,6 @@
 import { json2yaml } from "https://deno.land/x/json2yaml@v1.0.1/mod.ts";
 import { Path } from "../utils/path.ts";
-import { stage, watch } from "../app/args.ts";
+import { clear, stage, watch } from "../app/args.ts";
 import { createHash } from "https://deno.land/std@0.91.0/hash/mod.ts";
 // import { Datex } from "datex-core-legacy/no_init.ts";
 import { UIXRunner, runOptions } from "./runner.ts";
@@ -61,10 +61,11 @@ export default class LocalDockerRunner implements UIXRunner {
 			console.log(ESCAPE_SEQUENCES.GREEN + runOptions.endpoint + (Object.keys(runOptions.domains).length ? ` (${Object.keys(runOptions.domains).map(domain=>`https://${domain}`).join(", ")})` : '') +" is running in local docker container ("+id+")" + ESCAPE_SEQUENCES.RESET);
 			
 			if (runOptions.params.detach || id === "unknown id") {
-				return;
+				Deno.exit(0);
 			}
 		
 			else {
+				
 				await new Deno.Command("docker", {
 					args: [
 						"logs",
@@ -95,6 +96,7 @@ export default class LocalDockerRunner implements UIXRunner {
 		const args = [];
 		if (watch) args.push("--watch");
 		if (verboseArg) args.push("-v")
+		if (clear) args.push("--clear");
 
 		const dockerCompose = {
 			version: "3",

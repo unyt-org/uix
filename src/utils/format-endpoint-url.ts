602307d63173d1032007fe4a29c546c7ce9a3b54
benStre <strehle.benedikt@gmail.com>
2024-01-30 01:23:52 +0100

    outsource src/utils/format-endpoint-url.ts

--- src/utils/format-endpoint-url.ts
+++
@@ -1,8 +0,0 @@
-import type { Datex } from "datex-core-legacy/mod.ts";
-
-export function formatEndpointURL(endpoint:Datex.Endpoint) {
-	const endpointName = endpoint.toString();
-	if (endpointName.startsWith("@+")) return `${endpointName.replace("@+","")}.unyt.app`
-	else if (endpointName.startsWith("@@")) return `${endpointName.replace("@@","")}.unyt.app`
-	else if (endpointName.startsWith("@")) return `${endpointName.replace("@","")}.unyt.me`
-}

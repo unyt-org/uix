8d34090d98c716eb90224137512862304764f642
benStre <strehle.benedikt@gmail.com>
2024-03-02 14:32:14 +0100

    test swc version

--- src/server/transpiler.ts
+++ src/server/transpiler.ts
@@ -10,8 +10,11 @@ const copy = client_type === "deno" ? (await import("https://deno.land/std@0.160
 const walk = client_type === "deno" ? (await import("https://deno.land/std@0.177.0/fs/mod.ts")).walk : null;
 const sass = client_type === "deno" ? (await import("https://deno.land/x/denosass@1.0.6/mod.ts")).default : null;
 
+import { transformSync } from "npm:@swc/core@1.3.101";
+
+console.log("imported everything in transpiler")
 // TODO: investigate/bug report: later versions lead to Segfault in docker containers with deno 1.41
-const {transformSync} = client_type === "deno" ? await import("npm:@swc/core@1.3.101") : {transformSync:undefined};
+// const {transformSync} = client_type === "deno" ? await import("npm:@swc/core@1.3.101") : {transformSync:undefined};
 
 const logger = new Datex.Logger("transpiler");
 

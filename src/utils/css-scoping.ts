c21678410df68846156c95152c194f6642bb2e56
benStre <strehle.benedikt@gmail.com>
2024-04-07 20:15:26 +0200

    refactor css scoping using nested root selector

--- src/utils/css-scoping.ts
+++ src/utils/css-scoping.ts
@@ -1,30 +1,18 @@
 /**
- * Add a scope selector everywhere in a CSS document
+ * Wrap a CSS document with a custom scope selector
  * @param css css document
  * @param scope scope selector
  * @returns 
  */
 export function addCSSScopeSelector(css: string, scope: string) {
-	const scopedCSS = css.replace(/^[^@\n]+{[^}]*}/gm, (part) => {
-		if (part.match(/^(to|from|\d+%|[\s,\n])+{[^}]*}$/)) return part; // is inside @keyframe (e.g. "50% {}""), ignore
-		else {
-			// for each selectors (e.g. ':host, a#b'), add the scope, e.g. 'scope, scope a#b'
-			return part.replace(/^[^@\n]+(?={)/, (s) => {
-				const selectors = s.split(/, */g).map(selector => 
-					(selector.includes(":host") || selector.includes(":root")) ? 
-						selector
-							.trimEnd()
-							.replace(/\:(host|root)\s*\:host-context/g, ':host-context')
-							.replace(/\:(host|root)(?![-\w])/g, scope) : 
-						scope + ' ' + selector.trimEnd()
-					);
-				return selectors.join(", ") + ' '
-			})
-		}
-	});
-	return scopedCSS;
+	return `${scope} {\n${
+		'    ' + 
+		css
+			.replaceAll(':host', '&')
+			.replaceAll('\n', '\n    ')
+	}\n}`;
 }
 
 export function addCSSScope(css: string) {
-	return "@scope {\n" + css + "\n}";
+	return addCSSScopeSelector(css, "@scope")
 }

b31c07429adf849de16ad2ffbd40c59987362485
Jonas Strehle <jonas@strehles.info>
2024-10-14 22:18:25 +0200

    Fix * of config search

--- src/themes/tailwindcss.ts
+++ src/themes/tailwindcss.ts
@@ -88,7 +88,7 @@ export const tailwindcss = {
 			Deno.writeTextFileSync(inFile.normal_pathname, '@tailwind base;\n@tailwind components;\n@tailwind utilities;')
 		}
 		if (!configFile.fs_exists) {	
-			Deno.writeTextFileSync(configFile.normal_pathname, 'export default {\n  content: ["**/*.{html,tsx,ts,jsx,js}"],\n  theme: {\n    extend: {},\n  },\n  plugins: [],\n}')
+			Deno.writeTextFileSync(configFile.normal_pathname, 'export default {\n  content: ["./**/*.{html,tsx,ts,jsx,js}"],\n  theme: {\n    extend: {},\n  },\n  plugins: [],\n}')
 		}
 
 		try {
@@ -99,6 +99,8 @@ export const tailwindcss = {
 			const args =  [
 				"-i",
 				inFile.normal_pathname,
+				"-c",
+				configFile.normal_pathname,
 				"-o",
 				outFile.normal_pathname
 			]

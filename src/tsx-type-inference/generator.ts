608c991b2f530bfe9b1be33e9505a3e4f0e1d3c2
Jonas Strehle <jonas@strehles.info>
2025-05-12 21:41:29 +0200

    Remove logs

--- src/tsx-type-inference/generator.ts
+++ src/tsx-type-inference/generator.ts
@@ -376,12 +376,9 @@ export class TSXTypeInferenceGenerator {
 				hasRefMarker = requiredType!.types.some((type) => {
 					return this.#isRef(type)
 				});
-				console.log(node.parent.getText(), hasRefMarker, requiredType!.types.map((type) => this.#typeChecker.typeToString(type)));
-
 			}
 			else {
 				hasRefMarker = requiredType ? this.#isRef(requiredType) : false;
-				console.log(node.parent.getText(), hasRefMarker, requiredType ? this.#typeChecker.typeToString(requiredType) : "unknown");
 			}
 
 			// optimization: skip ref for internal elements if literal value (they all accept either refs or const values, so we can just skip them)
